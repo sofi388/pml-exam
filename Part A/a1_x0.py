89df84191531c8d45f6424850a75ca93020f3f8f
sofi388 <vereshchagina.sofia@outlook.com>
2025-01-15 13:24:05 +0100

    Update solution A1

--- Part A/a1_x0.py
+++ Part A/a1_x0.py
@@ -1,4 +1,4 @@
-# This UNET-style prediction model was originally included as part of the Score-based generative modelling tutorial 
+# This UNET-style prediction model was originally included as part of the Score-based generative modelling tutorial
 # by Yang Song et al: https://colab.research.google.com/drive/120kYYBOVa1i0TD85RjlEkFjaWDxSFUx3?usp=sharing
 
 import torch
@@ -7,10 +7,10 @@ import torch.nn.functional as F
 import numpy as np
 
 class GaussianFourierProjection(nn.Module):
-  """Gaussian random features for encoding time steps."""  
+  """Gaussian random features for encoding time steps."""
   def __init__(self, embed_dim, scale=30.):
     super().__init__()
-    # Randomly sample weights during initialization. These weights are fixed 
+    # Randomly sample weights during initialization. These weights are fixed
     # during optimization and are not trainable.
     self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
   def forward(self, x):
@@ -27,103 +27,104 @@ class Dense(nn.Module):
     return self.dense(x)[..., None, None]
 
 
-class ScoreNet(nn.Module):
-  """A time-dependent score-based model built upon U-Net architecture."""
-
-  def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256):
-    """Initialize a time-dependent score-based network.
-
-    Args:
-      marginal_prob_std: A function that takes time t and gives the standard
-        deviation of the perturbation kernel p_{0t}(x(t) | x(0)).
-      channels: The number of channels for feature maps of each resolution.
-      embed_dim: The dimensionality of Gaussian random feature embeddings.
-    """
-    super().__init__()
-    # Gaussian random feature embedding layer for time
-    self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
-         nn.Linear(embed_dim, embed_dim))
-    # Encoding layers where the resolution decreases
-    self.conv1 = nn.Conv2d(1, channels[0], 3, stride=1, bias=False)
-    self.dense1 = Dense(embed_dim, channels[0])
-    self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
-    self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False)
-    self.dense2 = Dense(embed_dim, channels[1])
-    self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
-    self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
-    self.dense3 = Dense(embed_dim, channels[2])
-    self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
-    self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
-    self.dense4 = Dense(embed_dim, channels[3])
-    self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])    
-
-    # Decoding layers where the resolution increases
-    self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride=2, bias=False)
-    self.dense5 = Dense(embed_dim, channels[2])
-    self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
-    self.tconv3 = nn.ConvTranspose2d(channels[2] + channels[2], channels[1], 3, stride=2, bias=False, output_padding=1)    
-    self.dense6 = Dense(embed_dim, channels[1])
-    self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
-    self.tconv2 = nn.ConvTranspose2d(channels[1] + channels[1], channels[0], 3, stride=2, bias=False, output_padding=1)    
-    self.dense7 = Dense(embed_dim, channels[0])
-    self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
-    self.tconv1 = nn.ConvTranspose2d(channels[0] + channels[0], 1, 3, stride=1)
-    
-    # The swish activation function
-    self.act = lambda x: x * torch.sigmoid(x)
-    self.marginal_prob_std = marginal_prob_std
-  
-  def forward(self, x, t): 
-    # Obtain the Gaussian random feature embedding for t   
-    embed = self.act(self.embed(t))    
-    # Encoding path
-    h1 = self.conv1(x)    
-    ## Incorporate information from t
-    h1 += self.dense1(embed)
-    ## Group normalization
-    h1 = self.gnorm1(h1)
-    h1 = self.act(h1)
-    h2 = self.conv2(h1)
-    h2 += self.dense2(embed)
-    h2 = self.gnorm2(h2)
-    h2 = self.act(h2)
-    h3 = self.conv3(h2)
-    h3 += self.dense3(embed)
-    h3 = self.gnorm3(h3)
-    h3 = self.act(h3)
-    h4 = self.conv4(h3)
-    h4 += self.dense4(embed)
-    h4 = self.gnorm4(h4)
-    h4 = self.act(h4)
-
-    # Decoding path
-    h = self.tconv4(h4)
-    ## Skip connection from the encoding path
-    h += self.dense5(embed)
-    h = self.tgnorm4(h)
-    h = self.act(h)
-    h = self.tconv3(torch.cat([h, h3], dim=1))
-    h += self.dense6(embed)
-    h = self.tgnorm3(h)
-    h = self.act(h)
-    h = self.tconv2(torch.cat([h, h2], dim=1))
-    h += self.dense7(embed)
-    h = self.tgnorm2(h)
-    h = self.act(h)
-    h = self.tconv1(torch.cat([h, h1], dim=1))
-
-    # Normalize output
-    h = h / self.marginal_prob_std(t)[:, None, None, None]
-    return h
 
 
+class ScoreNetPredX0(nn.Module):
+    """Score-based model predicting x0 instead of noise (ε)."""
+    def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256):
+        super().__init__()
+        # Gaussian random feature embedding layer for time
+        self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
+                                   nn.Linear(embed_dim, embed_dim))
+        # Encoding layers where the resolution decreases
+        self.conv1 = nn.Conv2d(1, channels[0], 3, stride=1, bias=False)
+        self.dense1 = Dense(embed_dim, channels[0])
+        self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
+        self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False)
+        self.dense2 = Dense(embed_dim, channels[1])
+        self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
+        self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
+        self.dense3 = Dense(embed_dim, channels[2])
+        self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
+        self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
+        self.dense4 = Dense(embed_dim, channels[3])
+        self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])
+
+        # Decoding layers where the resolution increases
+        self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride=2, bias=False)
+        self.dense5 = Dense(embed_dim, channels[2])
+        self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
+        self.tconv3 = nn.ConvTranspose2d(channels[2] + channels[2], channels[1], 3, stride=2, bias=False, output_padding=1)
+        self.dense6 = Dense(embed_dim, channels[1])
+        self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
+        self.tconv2 = nn.ConvTranspose2d(channels[1] + channels[1], channels[0], 3, stride=2, bias=False, output_padding=1)
+        self.dense7 = Dense(embed_dim, channels[0])
+        self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
+        self.tconv1 = nn.ConvTranspose2d(channels[0] + channels[0], 1, 3, stride=1)
+
+        self.act = lambda x: x * torch.sigmoid(x)
+        self.marginal_prob_std = marginal_prob_std
+
+    def forward(self, x, t):
+        # Obtain the Gaussian random feature embedding for t
+        embed = self.act(self.embed(t))
+        # Encoding path
+        h1 = self.conv1(x)
+        h1 += self.dense1(embed)
+        h1 = self.gnorm1(h1)
+        h1 = self.act(h1)
+        h2 = self.conv2(h1)
+        h2 += self.dense2(embed)
+        h2 = self.gnorm2(h2)
+        h2 = self.act(h2)
+        h3 = self.conv3(h2)
+        h3 += self.dense3(embed)
+        h3 = self.gnorm3(h3)
+        h3 = self.act(h3)
+        h4 = self.conv4(h3)
+        h4 += self.dense4(embed)
+        h4 = self.gnorm4(h4)
+        h4 = self.act(h4)
+
+        # Decoding path
+        h = self.tconv4(h4)
+        h += self.dense5(embed)
+        h = self.tgnorm4(h)
+        h = self.act(h)
+        h = self.tconv3(torch.cat([h, h3], dim=1))
+        h += self.dense6(embed)
+        h = self.tgnorm3(h)
+        h = self.act(h)
+        h = self.tconv2(torch.cat([h, h2], dim=1))
+        h += self.dense7(embed)
+        h = self.tgnorm2(h)
+        h = self.act(h)
+        h = self.tconv1(torch.cat([h, h1], dim=1))
+
+        # Return predicted x0 directly
+        return h
+
+
+# Modify the loss to compare both approaches
+
+def loss_fn_pred_x0(pred_x0, x0):
+    """Loss function for predicting x0"""
+    return F.mse_loss(pred_x0, x0)
+
+
+def loss_fn_pred_noise(pred_noise, true_noise):
+    """Loss function for predicting the noise (ε)"""
+    return F.mse_loss(pred_noise, true_noise)
+
+
+# Use either `ScoreNetPredX0` or the standard `ScoreNet` based on the experiment
 
 # ExponentialMovingAverage implementation as used in pytorch vision
 # https://github.com/pytorch/vision/blob/main/references/classification/utils.py#L159
 
 # BSD 3-Clause License
 
-# Copyright (c) Soumith Chintala 2016, 
+# Copyright (c) Soumith Chintala 2016,
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
@@ -150,7 +151,7 @@ class ScoreNet(nn.Module):
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    
+
 class ExponentialMovingAverage(torch.optim.swa_utils.AveragedModel):
     """Maintains moving averages of model parameters using an exponential decay.
     ``ema_avg = decay * avg_model_param + (1 - decay) * model_param``
@@ -182,19 +183,19 @@ class DDPM(nn.Module):
         network: nn.Module
             The inner neural network used by the diffusion process. Typically a Unet.
         beta_1: float
-            beta_t value at t=1 
+            beta_t value at t=1
         beta_T: [float]
             beta_t value at t=T (last step)
         T: int
             The number of diffusion steps.
         """
-        
+
         super(DDPM, self).__init__()
 
         # Normalize time input before evaluating neural network
         # Reshape input into image format and normalize time value before sending it to network model
         self._network = network
-        self.network = lambda x, t: (self._network(x.reshape(-1, 1, 28, 28), 
+        self.network = lambda x, t: (self._network(x.reshape(-1, 1, 28, 28),
                                                    (t.squeeze()/T))
                                     ).reshape(-1, 28*28)
 
@@ -205,12 +206,12 @@ class DDPM(nn.Module):
         self.register_buffer("beta", torch.linspace(beta_1, beta_T, T+1))
         self.register_buffer("alpha", 1-self.beta)
         self.register_buffer("alpha_bar", self.alpha.cumprod(dim=0))
-        
+
 
     def forward_diffusion(self, x0, t, epsilon):
         '''
         q(x_t | x_0)
-        Forward diffusion from an input datapoint x0 to an xt at timestep t, provided a N(0,1) noise sample epsilon. 
+        Forward diffusion from an input datapoint x0 to an xt at timestep t, provided a N(0,1) noise sample epsilon.
         Note that we can do this operation in a single step
 
         Parameters
@@ -218,7 +219,7 @@ class DDPM(nn.Module):
         x0: torch.tensor
             x value at t=0 (an input image)
         t: int
-            step index 
+            step index
         epsilon:
             noise sample
 
@@ -226,20 +227,12 @@ class DDPM(nn.Module):
         -------
         torch.tensor
             image at timestep t
-        ''' 
+        '''
 
-        # TODO: Define the mean and std variables
-        
-        # Mean is sqrt(alpha_bar[t]) * x0
-        mean = torch.sqrt(self.alpha_bar[t])[:, None, None, None] * x0
-        # Standard deviation is sqrt(1 - alpha_bar[t])
-        std = torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None]
-        
-        # Add noise epsilon
-        xt = mean + std * epsilon
-        return xt
-    
-        # return mean + std*epsilon
+        mean = torch.sqrt(self.alpha_bar[t])*x0
+        std = torch.sqrt(1 - self.alpha_bar[t])
+
+        return mean + std*epsilon
 
     def reverse_diffusion(self, xt, t, epsilon):
         """
@@ -261,18 +254,12 @@ class DDPM(nn.Module):
             image at timestep t-1
         """
 
-        # TODO: Define the mean and std variables
- 
-        # Calculate mean for the reverse step
-        mean = (xt - torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None] * epsilon) / torch.sqrt(self.alpha[t])[:, None, None, None]
-        # Standard deviation for the reverse step is sqrt(1 - alpha_bar[t-1])
-        std = torch.sqrt(1 - self.alpha_bar[t-1])[:, None, None, None]
-        
-        xt_1 = mean + std * epsilon
-        return xt_1
-        # return mean + std*epsilon
+        mean =  1./torch.sqrt(self.alpha[t]) * (xt - (self.beta[t])/torch.sqrt(1-self.alpha_bar[t])*self.network(xt, t))
+        std = torch.where(t>0, torch.sqrt(((1-self.alpha_bar[t-1]) / (1-self.alpha_bar[t]))*self.beta[t]), 0)
 
-    
+        return mean + std*epsilon
+
+
     @torch.no_grad()
     def sample(self, shape):
         """
@@ -286,21 +273,21 @@ class DDPM(nn.Module):
         Returns
         -------
         torch.tensor
-            sampled image            
+            sampled image
         """
-        
+
         # Sample xT: Gaussian noise
         xT = torch.randn(shape).to(self.beta.device)
 
         xt = xT
         for t in range(self.T, 0, -1):
             noise = torch.randn_like(xT) if t > 1 else 0
-            t = torch.tensor(t).expand(xt.shape[0], 1).to(self.beta.device)            
+            t = torch.tensor(t).expand(xt.shape[0], 1).to(self.beta.device)
             xt = self.reverse_diffusion(xt, t, noise)
 
         return xt
 
-    
+
     def elbo_simple(self, x0):
         """
         ELBO training objective (Algorithm 1 in Ho et al, 2020)
@@ -313,21 +300,21 @@ class DDPM(nn.Module):
         Returns
         -------
         float
-            ELBO value            
+            ELBO value
         """
 
         # Sample time step t
         t = torch.randint(1, self.T, (x0.shape[0],1)).to(x0.device)
-        
+
         # Sample noise
         epsilon = torch.randn_like(x0)
 
         # TODO: Forward diffusion to produce image at step t
         xt = self.forward_diffusion(x0, t, epsilon)
-        
+
         return -nn.MSELoss(reduction='mean')(epsilon, self.network(xt, t))
 
-    
+
     def loss(self, x0):
         """
         Loss function. Just the negative of the ELBO.
@@ -338,7 +325,7 @@ class DDPM(nn.Module):
 def train(model, optimizer, scheduler, dataloader, epochs, device, ema=True, per_epoch_callback=None):
     """
     Training loop
-    
+
     Parameters
     ----------
     model: nn.Module
@@ -369,8 +356,8 @@ def train(model, optimizer, scheduler, dataloader, epochs, device, ema=True, per
         ema_adjust = dataloader.batch_size * ema_steps / epochs
         ema_decay = 1.0 - 0.995
         ema_alpha = min(1.0, (1.0 - ema_decay) * ema_adjust)
-        ema_model = ExponentialMovingAverage(model, device=device, decay=1.0 - ema_alpha)                
-    
+        ema_model = ExponentialMovingAverage(model, device=device, decay=1.0 - ema_alpha)
+
     for epoch in range(epochs):
 
         # Switch to train mode
@@ -392,8 +379,8 @@ def train(model, optimizer, scheduler, dataloader, epochs, device, ema=True, per
             if ema:
                 ema_global_step_counter += 1
                 if ema_global_step_counter%ema_steps==0:
-                    ema_model.update_parameters(model)                
-        
+                    ema_model.update_parameters(model)
+
         if per_epoch_callback:
             per_epoch_callback(ema_model.module if ema else model)
 
@@ -405,11 +392,11 @@ epochs = 100
 batch_size = 256
 
 
-# Rather than treating MNIST images as discrete objects, as done in Ho et al 2020, 
+# Rather than treating MNIST images as discrete objects, as done in Ho et al 2020,
 # we here treat them as continuous input data, by dequantizing the pixel values (adding noise to the input data)
 # Also note that we map the 0..255 pixel values to [-1, 1], and that we process the 28x28 pixel values as a flattened 784 tensor.
 transform = transforms.Compose([
-    transforms.ToTensor(), 
+    transforms.ToTensor(),
     transforms.Lambda(lambda x: x + torch.rand(x.shape)/255),    # Dequantize pixel values
     transforms.Lambda(lambda x: (x-0.5)*2.0),                    # Map from [0,1] -> [-1, -1]
     transforms.Lambda(lambda x: x.flatten())
@@ -428,7 +415,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # different noise levels, such that the output can be rescaled.
 # Since we are predicting the noise (rather than the score), we
 # ignore this rescaling and just set std=1 for all t.
-mnist_unet = ScoreNet((lambda t: torch.ones(1).to(device)))
+mnist_unet = ScoreNetPredX0((lambda t: torch.ones(1).to(device)))
 
 # Construct model
 model = DDPM(mnist_unet, T=T).to(device)
@@ -442,24 +429,24 @@ scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.9999)
 
 def reporter(model):
     """Callback function used for plotting images during training"""
-    
+
     # Switch to eval mode
     model.eval()
 
     with torch.no_grad():
         nsamples = 10
         samples = model.sample((nsamples,28*28)).cpu()
-        
+
         # Map pixel values back from [-1,1] to [0,1]
-        samples = (samples+1)/2 
+        samples = (samples+1)/2
         samples = samples.clamp(0.0, 1.0)
 
         # Plot in grid
         grid = utils.make_grid(samples.reshape(-1, 1, 28, 28), nrow=nsamples)
         plt.gca().set_axis_off()
         plt.imshow(transforms.functional.to_pil_image(grid), cmap="gray")
-        plt.show()    
+        plt.show()
 
 # Call training loop
-train(model, optimizer, scheduler, dataloader_train, 
+train(model, optimizer, scheduler, dataloader_train,
       epochs=epochs, device=device, ema=True, per_epoch_callback=reporter)
